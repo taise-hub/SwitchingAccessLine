7f26a31f4b3a22a3d2bda914ca49dbdca407c066
井上大誠 <t.base.8901@icloud.com>
2021-10-08 14:26:52 +0900

    新しいコントローラの実装

--- controller.py
+++ controller.py
@@ -12,15 +12,15 @@ class MyController(app_manager.RyuApp):
 
     def __init__(self, *args, **kwargs):
         super(MyController, self).__init__(*args, **kwargs)
-        user_role_dict = {'10.11.1.1': 1, '10.11.1.2': 2} # 1: doctor, 2: normal
-        application_dict = {25:'health_care'}
-        current_flows = {} #リアルタイムで流れているFlowのオブジェクトを格納 {dpid:[]}
-        current_flows_role = {} #リアルタイムで流れているFlowのIPアドレスとroleを格納
+        non_inference_flows = {} #リアルタイムで流れているFlowのオブジェクトを格納 {dpid:[]}
         
 
     # install the table-miss flow entry.
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
+        """
+        テーブルミスエントリーを追加します。
+        """
         datapath = ev.msg.datapath
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
@@ -30,6 +30,9 @@ class MyController(app_manager.RyuApp):
 
     # add flow entry.
     def add_flow(self, datapath, priority, match, actions):
+        """
+        フローを追加する関数です。
+        """
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
         inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
@@ -38,62 +41,85 @@ class MyController(app_manager.RyuApp):
                                 match=match, instructions=inst)
         datapath.send_msg(mod)
 
+    # packet in handler
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
+        """
+        パケットインハンドラーでは、最もセキュアな回線を選択するフローエントリを追加します。
+        後期推論のためにキュー上にフローを追加します。
+        """
         msg = ev.msg
         datapath = msg.datapath
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
-        
         dpid = datapath.id
-        self.current_flows.setdefault(dpid, {})
-        # self.current_flows_role.setdefault(dpid, {})
-
+        self.non_inference_flows.setdefault(dpid, [])
         pkt = packet.Packet(msg.data)
+        self.non_inference_flows[dpid].append(pkt)
+        self.logger.info("add non_inference_flow: %s", self.non_inference_flows[dpid])
         ipv4_pkt = pkt.get_protocol(ipv4.ipv4)
-        tcp_pkt = pkt.get_protocol(tcp.tcp)
+        ipv4_src = ipv4_pkt.src
+        ipv4_dst = ipv4_pkt.dst
+        self.logger.info("packet in【%s】: %s to %s",dpid, ipv4_src, ipv4_dst)
+        match = parser.OFPMatch(ipv4_src=ipv4_src, ipv4_dst=ipv4_dst)
+        in_port = msg.match['in_port']
+        out_port = 3 # 最もセキュアな回線につながっているポート
+        actions = [parser.OFPActionOutput(out_port)]
+        self.add_flow(datapath, 1, match, actions)
+        out = parser.OFPPacketOut(datapath=datapath,
+                                  buffer_id=ofproto.OFP_NO_BUFFER,
+                                  in_port=in_port,
+                                  actions=actions,
+                                  data=msg.data)
+        datapath.send_msg(out)
 
-        ipv4_src = ipv4_pkt.src #INFO: 簡略化のため、送信元IPアドレスのみをロールの特定に利用する。
-        role = self.user_role_dict.get(ipv4_src) # Make role inferences from IP address
-        self.current_flows[dpid][ev.msg] = role # 流れているフローの情報を全て保持する。
-
-        low_priority_flows = self.compare_priorities(dpid, role) # Compare priorities by role
-        if low_priority_flows:
-            for flow in low_priority_flows:       
-                pkt = packet.Packet(flow) 
-                ipv4_pkt = pkt.get_protocol(ipv4.ipv4)
-                tcp_pkt = pkt.get_protocol(tcp.tcp)
-                match = parser.OFPMatch(
-                    ipv4_src= ipv4_pkt.src,
-                    ipv4_dst= ipv4_pkt.dst,
-                    tcp_dst= tcp_pkt.dst_port)
-                self.drop_flow(datapath, match)
-
-        tcp_dst_port = tcp_pkt.dst_port
-        application = self.application_dict.get(tcp_dst_port)
-        app_request = ApplicationRequest(application)
-
-        #TODO: ※(A)アクセス回線の状況を取得(port statistics request)
-        #TODO: 使えないアクセス回線の除外(※(A)で取得したアクセス回線状況を利用する)
-        #TODO: 残った候補に対してTOPSISアルゴリズムを適用して、アクセス回線を決定する。
-        #TODO: flow entryの追加を行う。
-        #TODO: low_priority_flowsをcurrent_flows_roleを使って優先度順に並び替える
-        #TODO: low_priority_flowsの中で優先度の高いものから、繰り返し、最適なアクセス回線を決定しフローエントリに追加する。アクセス回線を追加するごとにDropにしいていたFlow entryを削除する。
-    
-    # return value: [FlowObject1, FlowObject2,...]
-    def compare_priorities(self, dpid, current_role):
-        low_priority_flows = []
-        for flow in self.current_flows[dpid]:
-            if self.current_flows[dpid][flow] < current_role:
-                low_priority_flows.append(flow)
-        return low_priority_flows
+    @set_ev_cls(ofp_event.EventOFPPortStatsReply, MAIN_DISPATCHER)
+    def port_stats_reply_handler(self, ev):
+        """
+        port statistic replyを受け取るハンドラーです。
+        このハンドラーは、統計情報を更新し未遂論のフローがキューに残っていれば推論を行い、フローエントリーを更新します。
+        """
+        ports = []
+        for stat in ev.msg.body:
+            ports.append('port_no=%d '
+                        'rx_packets=%d tx_packets=%d '
+                        'rx_bytes=%d tx_bytes=%d '
+                        'rx_dropped=%d tx_dropped=%d '
+                        'rx_errors=%d tx_errors=%d '
+                        'rx_frame_err=%d rx_over_err=%d rx_crc_err=%d '
+                        'collisions=%d duration_sec=%d duration_nsec=%d' %
+                        (stat.port_no,
+                        stat.rx_packets, stat.tx_packets,
+                        stat.rx_bytes, stat.tx_bytes,
+                        stat.rx_dropped, stat.tx_dropped,
+                        stat.rx_errors, stat.tx_errors,
+                        stat.rx_frame_err, stat.rx_over_err,
+                        stat.rx_crc_err, stat.collisions,
+                        stat.duration_sec, stat.duration_nsec))
+        self.logger.debug('PortStats: %s', ports)
 
     def drop_flow(self, datapath, match):
+        """
+        フローエントリをドロップする関数です。
+        利用するかは未定です。
+        """
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
         mod = parser.OFPFlowMod(datapath=datapath, command=ofproto.OFPFC_DELETE, match=match)
         datapath.send_msg(mod)
         return
+
+    def send_port_stats_request(self, datapath):
+        """
+        OFSに各ポートの統計情報をリクエストする関数です。
+        一秒毎にポーリングする予定です。
+        """
+        ofp = datapath.ofproto
+        ofp_parser = datapath.ofproto_parser
+        req = ofp_parser.OFPPortStatsRequest(datapath, 0, ofp.OFPP_ANY) #全ポートの統計情報を収集
+        datapath.send_msg(req)
+
+
 
 class ApplicationRequest:
     name = None
